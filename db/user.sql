753ec5de37b9e4db2be40e6bdb52c8f9b928dcfe
Mutix <muti@mutix.tr>
2026-07-23 23:44:24 +0300

    Delete db directory

--- db/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXT DEFAULT 'user' CHECK(role IN ('user', 'mod', 'admin')),
-    is_verified INTEGER DEFAULT 0,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
